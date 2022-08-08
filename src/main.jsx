27c1f5e9e45aaa575309420942c5c70eba616133
Renan <renan.fachin@hotmail.com>
2022-08-08 14:30:50 -0300

    Inicio da estrutura base do projeto, conceitos de import e export de funções e introdução aos conceitos de jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
+import { Details } from './pages/Details' 
+// {} para importação sem o default, precisa usar exatamente o mesmo nome dentro da função
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Details />
   </React.StrictMode>
 )
---
+++ src/pages/Details.jsx
@@ -0,0 +1,9 @@
+// é necessário exportar a função (export)
+export function Details(){
+
+  return (
+    // Onde estará o conteúdo da interface
+    <h1>Hello world!</h1>
+  )
+}
+
