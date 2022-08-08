968085ceb2c01ca1911336f3f318192533846a86
Renan <renan.fachin@hotmail.com>
2022-08-08 16:58:56 -0300

    Criação de uma styled component chamada theme para gerenciar e padronizar estilizações globais da aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,13 @@ import ReactDOM from 'react-dom/client'
 import { Details } from './pages/Details' // está buscando o index de dentro da pasta details
 // {} para importação sem o default, precisa usar exatamente o mesmo nome dentro da função
 
+import { ThemeProvider } from 'styled-components'
+import theme from '.styles/theme' // importando o theme criado
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>
 )
