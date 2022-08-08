41b28e4f21b73122ce687a85d84bb3b130058b3f
Renan <renan.fachin@hotmail.com>
2022-08-08 17:07:09 -0300

    Criado um arquivo de estilização global e feito o import para o main

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import { Details } from './pages/Details' // está buscando o index de dentro da
 // {} para importação sem o default, precisa usar exatamente o mesmo nome dentro da função
 
 import { ThemeProvider } from 'styled-components'
-import theme from '.styles/theme' // importando o theme criado
+import theme from './styles/theme' // importando o theme criado
+import GlobalStyle from './styles/global' // importando o globalstyles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Details />
     </ThemeProvider>
   </React.StrictMode>
