3a04d7353f2fc07b7fa1f77c5b5fd58651adac27
Renan <renan.fachin@hotmail.com>
2022-08-08 15:02:43 -0300

    1 componente retorna apenas um único elemento. Neste caso, o fragment vem para ajustar esta necessidade. Neste commit, foi feito o uso do fragment

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,16 @@
+// Fazendo o import do fragment de dentro do react - import { Fragment } from 'react'
+// Só é necessário este import caso se deseje utilizar <Frament> </Fragment> como caixa de conteúdo
+// Se não quiser fazer o import, pode se utilizar desta propriedade usando apenas <> </>
+
 // é necessário exportar a função (export)
 export function Details(){
 
   return (
     // Onde estará o conteúdo da interface
+    <>
     <h1>Hello world!</h1>
+    <span>Renan</span>
+    </>
   )
 }
 
