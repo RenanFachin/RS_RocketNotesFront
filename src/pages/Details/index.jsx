c066f9c60bb41f20ef7173296f0b20e01978c5e0
Renan <renan.fachin@hotmail.com>
2022-08-08 16:51:28 -0300

    Feita a instalação do StyleComponent para fazer o CSS-IN-JS e suas imports para funcionamento

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,18 +2,19 @@
 // Só é necessário este import caso se deseje utilizar <Frament> </Fragment> como caixa de conteúdo
 // Se não quiser fazer o import, pode se utilizar desta propriedade usando apenas <> </>
 
-// Fazendo o IMPORT do css
-import "./styles.css"
-
+// Fazendo o IMPORT do styled component
+import { Container } from './styles'
+ 
 // é necessário exportar a função (export)
 export function Details(){
 
   return (
     // Onde estará o conteúdo da interface
-    <>
+    // Fragment <> para agrupar os elementos
+    <Container>
     <h1>Hello world!</h1>
     <span>Renan</span>
-    </>
+    </Container>
   )
 }
 
