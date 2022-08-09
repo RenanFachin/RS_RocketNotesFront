2632285f071996658a965264313a2627a2100c74
Renan <renan.fachin@hotmail.com>
2022-08-09 15:46:07 -0300

    Adicionado uma função PROFILE dentro do componente HEADER para definição de alguns elementos. Elementos estilizados: div, img, span e strong

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,20 @@
 // criando a estrutura para receber o styles.js
 
-import { Container } from './styles'; // Fazendo o import do styles.js que tbm está dentro da pasta HEADER
+import { Container, Profile } from './styles'; // Fazendo o import do styles.js que tbm está dentro da pasta HEADER
 
 export function Header(){
     return(
         <Container>
 
+            <Profile>
+                <img src="https://github.com/RenanFachin.png" alt="Foto do usuário" />
+
+                <div>
+                    <span>Bem-vindo</span>
+                    <strong>Renan Fachin</strong>
+                </div>
+            </Profile>
+            
         </Container>
     )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -16,6 +16,36 @@ export const Container = styled.header`
     justify-content: space-between;
 
     padding: 0 80px;
+`;
 
-    background: red;
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
+
+    //  o simbolo de > é para garantir que somente a img que está dentro da profile será estilizada
+    > img {
+        width: 56px;
+        height: 56px;
+        border-radius: 50%; // deixando a imagem redonda
+    }
+
+    //  o simbolo de > é para garantir que somente a div que está dentro da profile será estilizada
+    > div {
+        display: flex;
+        flex-direction: column;
+        margin-left: 16px;
+        line-height: 24px;
+
+        // modificando a span dentro da div que está dentro de profile, não precisa de > pois ele já está em um bom nível de especificidade
+        span {
+            font-size: 14px;
+            color: ${({theme}) => theme.COLORS.GRAY_100}
+        }
+
+        strong {
+            font-size: 16px;
+            color: ${({theme}) => theme.COLORS.WHITE}
+        }
+    }
+
 `;
