a9559dce8f58a21ef08e772ee593dd2773ba4881
Renan <renan.fachin@hotmail.com>
2022-08-10 15:59:01 -0300

    Inicio do styles.js referente ao home + dicas de produtividade

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,14 @@
+// Importando o styled Components
+import styled from "styled-components";
+
+export const Container = styled.view``;
+
+export const Brand = styled.view``;
+
+export const Menu = styled.view``;
+
+export const Search = styled.view``;
+
+export const Content = styled.view``;
+
+export const NewNot = styled.view``;
