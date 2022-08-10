c8c70aa1360e9eb3abaefc519384b58d3f8d7625
Renan <renan.fachin@hotmail.com>
2022-08-10 16:04:53 -0300

    Feita a estilização parcial do container da home. Feita a definição dos grid templates areas, grid columns e grid row

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,14 +1,30 @@
 // Importando o styled Components
 import styled from "styled-components";
 
-export const Container = styled.view``;
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
 
-export const Brand = styled.view``;
+    display: grid;
+    grid-template-columns: 250px auto; // separando em duas colunas, uma de 250px (será o menu) e outra que ocupará todo o restante
+    grid-template-rows: 105px 128px auto 64px;
+    //105px será o cabeçalho, 128px será o search, auto o restante do conteudo e 64px para o botão do final do menu lateral
 
-export const Menu = styled.view``;
+    grid-template-areas: 
+    "brand header"
+    "menu search"
+    "menu content"
+    "newnote content";
 
-export const Search = styled.view``;
+    background-color: ${({theme})=> theme.COLORS.BACKGROUND_800};
+`;
 
-export const Content = styled.view``;
+export const Brand = styled.div``;
 
-export const NewNot = styled.view``;
+export const Menu = styled.ul``;
+
+export const Search = styled.div``;
+
+export const Content = styled.div``;
+
+export const NewNot = styled.button``;
