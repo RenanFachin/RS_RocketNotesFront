6a88e599b45405dbb5b1f58319e7acbf23a270e1
Renan <renan.fachin@hotmail.com>
2022-08-10 15:55:38 -0300

    Inicio da estrutura da página HOME

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -52,6 +52,7 @@ export function Details(){
         <Button title="Voltar" />
         </Content>
       </main>
+      
     </Container>
   )
 }
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,25 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
+
+// Importando o componente - HEADER
+import { Header } from '../../components/Header';
+
+export function Home(){
+    return (
+        <Container>
+            <Brand>
+                <h1>RocketNotes</h1>
+            </Brand>
+            
+            <Header/>
+
+            <Menu></Menu>
+
+            <Search></Search>
+
+            <Content></Content>
+
+            <NewNote></NewNote>
+
+        </Container>
+    )
+}
