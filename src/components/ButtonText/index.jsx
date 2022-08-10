170614cb517b9f6d0984732b61c0b05462baa435
Renan <renan.fachin@hotmail.com>
2022-08-10 17:51:27 -0300

    Criado um menu lateral com os itens. Implementação de uma condição de isActive para saber em qual página o usuário esta no momento

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -1,9 +1,11 @@
 import { Container } from './style'
 
-export function ButtonText({ title, ...rest }){
+// isActive = false  -> para atribuir um valor padrão, caso não seja informado pela página
+export function ButtonText({ title, isActive = false, ...rest }){
     return(
         <Container 
             type="button"
+            isActive={isActive}
             {...rest}
         >
             {title}
--- src/components/ButtonText/style.js
+++ src/components/ButtonText/style.js
@@ -2,7 +2,9 @@ import styled from "styled-components";
 
 export const Container = styled.button`
     background: none;
-    color: ${({ theme }) => theme.COLORS.ORANGE};
+    
+    color: ${({ theme, isActive }) => isActive ? theme.COLORS.ORANGE : theme.COLORS.GRAY_100 };
+    // Criando uma condição, se tiver ativo é laranja
 
     border: none;
     font-size: 16px;
