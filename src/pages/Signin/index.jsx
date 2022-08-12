ad405614b82b02c27bb1226af891590cdb43ff7c
Renan <renan.fachin@hotmail.com>
2022-08-12 11:09:00 -0300

    Inicio da criação da página de SignIn. Criada pasta SignIn dentro das pages junto com a estrutura base de funcionamento

---
+++ src/pages/Signin/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'; // Importando o elemento Container criado no style.js
+
+// Criando a função signIn que vai conter os elementos de styles e será utilizado para renderizar a página
+export function SignIn(){
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
---
+++ src/pages/Signin/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"; // Importaçao inicial do styled
+
+// Criando uma constante container (sendo uma div) e já exportando ela para ser utilzada no index.jsx
+export const Container = styled.div`
+    height: 100vh;
+    
+    display: flex;
+    align-items: stretch;
+`;
