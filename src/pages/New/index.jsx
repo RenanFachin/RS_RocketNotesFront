900d75261f0532db3d4be4e7d66958698118d6fe
Renan <renan.fachin@hotmail.com>
2022-08-12 18:11:41 -0300

    Adicionada na pagina NEW o componente TEXTAREA criado anteriormente

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,6 @@
 import {Header} from '../../components/Header'
 import {Input} from '../../components/Input'
+import {TextArea} from '../../components/TextArea'
 
 import {Container, Form} from './styles'
 
@@ -18,6 +19,7 @@ export function New(){
                     </header>
 
                     <Input placeholder="Título" />
+                    <TextArea placeholder="Observações"/>
                 </Form>
                 
             </main>
