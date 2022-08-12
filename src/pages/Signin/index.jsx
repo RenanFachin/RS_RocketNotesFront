25ec7ddb91fec72a01a43396451838051bdb7ec7
Renan <renan.fachin@hotmail.com>
2022-08-12 11:17:40 -0300

    Criada a estrutura base da página SignIn e feito os imports dos componentes input e button

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,10 +1,37 @@
 import { Container } from './styles'; // Importando o elemento Container criado no style.js
+import { FiMail, FiLock} from 'react-icons/fi' // Fazendo o import de um ícone da bibliteca react-icons
+
+import { Input } from '../../components/Input'; // Importando o componente INPUT
+import { Button } from '../../components/Button'; // Importando o componente BUTTON
+
 
 // Criando a função signIn que vai conter os elementos de styles e será utilizado para renderizar a página
 export function SignIn(){
     return (
         <Container>
-            
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+                <h2>Faça seu login</h2>
+
+                <Input
+                    placeholder = "E-mail"
+                    type= "text"
+                    icon={FiMail}
+                />
+
+                <Input
+                    placeholder = "Senha"
+                    type= "password"
+                    icon={FiLock}
+                />
+
+                <Button>
+                    Entrar
+                </Button>
+
+            </Form>
         </Container>
     )
 }
