64887da37ee530744dc55c72ae67a7eeb67115ae
Renan <renan.fachin@hotmail.com>
2022-08-12 15:16:22 -0300

    Criada a interface de perfil do usuário

--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -27,7 +27,52 @@ export const Container = styled.div`
 
 export const Form = styled.form`
     max-width: 340px;
-    margin: -84px auto 0; // para centralizar e fazer ele subir um pouco no header
+    margin: -95px auto 0; // para centralizar e fazer ele subir um pouco no header  
 
-    
+    > div:nth-child(4){
+        margin-top: 24px;
+    }
+`;
+
+export const Avatar = styled.div`
+    position: relative;
+    margin: 0 auto 32px;
+    width: 186px;
+    height: 186px;
+
+
+    >img{
+        width: 186px;
+        height: 186px;
+
+        border-radius: 50%;
+    }
+
+
+    > label {
+        width: 48px;
+        height: 48px;
+        background-color: ${({theme})=> theme.COLORS.ORANGE};
+        border-radius: 50%;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        position: absolute;
+        bottom: 7px;
+        right: 7px;
+
+        cursor: pointer;
+
+        input {
+            display: none; // escondendo as coisas e deixando só o svg aparecendo
+        }
+
+        svg{
+            width: 20px;
+            height: 20px;
+            color: ${({theme})=> theme.COLORS.BACKGROUND_800};
+        }
+    }
 `;
