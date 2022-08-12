693663fbf3bf3abf969774300728dccc155fe391
Renan <renan.fachin@hotmail.com>
2022-08-12 11:11:15 -0300

    Alterado na main.jsx (que é quem controla o que será renderizado na raiz) para mostrar a página de SignIn para melhor estilização dela

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,14 @@ import theme from './styles/theme' // importando o theme criado
 import GlobalStyle from './styles/global' // importando o globalstyles
 
 
-import { Home } from './pages/Home' // está buscando o index de dentro da pasta details
+import { SignIn } from './pages/SignIn' // está buscando o index de dentro da pasta details
 // {} para importação sem o default, precisa usar exatamente o mesmo nome dentro da função
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home/>
+      <SignIn/>
     </ThemeProvider>
   </React.StrictMode>
 )
