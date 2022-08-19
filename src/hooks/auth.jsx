7e7f701a53c2e9b61d4acc3005a763eb4edd0191
Renan <renan.fachin@hotmail.com>
2022-08-19 14:14:23 -0300

    Finaliza a função de autenticação, guardando o token de autenticação e o user do usuário logado

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,5 +1,5 @@
 // Importando do react
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 // Importando a API
 import {api} from '../services/api'
@@ -9,6 +9,8 @@ export const AuthContext = createContext({});
 
 // O children vai ser as rotas da aplicação (lá do main). é o filho do authContext
 function AuthProvider({ children }){
+    // Sempre no inicio se cria os estados
+    const [data, setData] = useState({});
 
     // Função de autenticação recebe 2 parâmetros: email e password
     // Passado como objeto para a ordem não ter importância
@@ -18,7 +20,16 @@ function AuthProvider({ children }){
         try{
         // Fazendo requisição para a api
             const response = await api.post("/sessions", {email, password});
-            console.log(response)
+
+            // Destruturando o data para voltar somente os dados de user e o token de login
+            const { user, token } = response.data
+
+            // Inserindo um token de autenticação no cabeçalho de todas as requisições que usuário fará apartir de seu login
+            api.defaults.headers.authorization = `Bear ${token}`;
+
+            // Armazendando as informações
+            setData({user,token})
+
         } catch(error){
             if(error.response){
                 alert(error.response.data.message)
@@ -30,7 +41,7 @@ function AuthProvider({ children }){
     }
 
     return (
-        <AuthContext.Provider value={{ signIn }}>
+        <AuthContext.Provider value={{ signIn, user: data.user }}>
             {children}
         </AuthContext.Provider>
     )
