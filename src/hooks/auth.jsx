4ea0906a36c7d5498f47b94dd6f4bdd3613f2a4c
Renan <renan.fachin@hotmail.com>
2022-08-19 14:08:15 -0300

    :construction: Feat: Inicia a construção do signIn utilizando Hooks de autenticação e estados

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,13 +1,36 @@
 // Importando do react
 import { createContext, useContext } from "react";
 
+// Importando a API
+import {api} from '../services/api'
+
 // Criando um contexto com valor default um objeto vazio
 export const AuthContext = createContext({});
 
 // O children vai ser as rotas da aplicação (lá do main). é o filho do authContext
 function AuthProvider({ children }){
+
+    // Função de autenticação recebe 2 parâmetros: email e password
+    // Passado como objeto para a ordem não ter importância
+    async function signIn({ email, password }){
+
+        // Try e catch para fazer o tratamento de erros
+        try{
+        // Fazendo requisição para a api
+            const response = await api.post("/sessions", {email, password});
+            console.log(response)
+        } catch(error){
+            if(error.response){
+                alert(error.response.data.message)
+            } else{
+                alert("Não foi possível fazer o login")
+            }
+        }
+
+    }
+
     return (
-        <AuthContext.Provider value={{name: 'teste', email: 'teste@email.com' }}>
+        <AuthContext.Provider value={{ signIn }}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Container, Form, Background } from './styles'; // Importando o elemento Container criado no style.js
 import { FiMail, FiLock} from 'react-icons/fi' // Fazendo o import de um ícone da bibliteca react-icons
 
@@ -10,8 +12,19 @@ import { useAuth } from '../../hooks/auth'; // Importando o contexto criado
 
 // Criando a função signIn que vai conter os elementos de styles e será utilizado para renderizar a página
 export function SignIn(){
-    const data = useAuth(); // Acessando o hook criado
-    console.log("Meu contexto;", data);
+    
+    // Criando os estados
+    const [email, setEmail] = useState(""); // Começando vazio
+    const [password, setPassword] = useState(""); // Começando vazio
+
+    const { signIn } = useAuth(); // Acessando o hook criado
+
+    // função que será "disparada" quando um evento acontecer
+    function handleSignIn(){
+        signIn({ email, password })
+    }
+
+    
     return (
         <Container>
             <Form>
@@ -24,15 +37,20 @@ export function SignIn(){
                     placeholder = "E-mail"
                     type= "text"
                     icon={FiMail}
+                    // Capturando as mudanças que ocorrem no input e atribuindo em setEmail
+                    onChange={e=>setEmail(e.target.value)}
                 />
 
                 <Input
                     placeholder = "Senha"
                     type= "password"
                     icon={FiLock}
+                    // Capturando as mudanças que ocorrem no input e atribuindo em setPassword
+                    onChange={e=>setPassword(e.target.value)}
                 />
 
-                <Button title="Entrar" />
+                {/* Disparando a função quando acontecer um click no button */}
+                <Button title="Entrar" onClick={handleSignIn}/>
 
 
                 <Link to='/register'>
