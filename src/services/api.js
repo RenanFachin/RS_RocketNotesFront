3e6ff5324bf9450c9ac51a97241a2ac3aa0162b3
Renan <renan.fachin@hotmail.com>
2022-08-24 18:34:18 -0300

    Bug fix

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 // método create dentro do axios
 // Se passa um objeto para este método
 export const api = axios.create({
-    baseURL: "http://localhost:3333"
+    baseURL: "https://rocketnotes-api182.herokuapp.com"
     // endereço do backend, para não precisar repetir em todas as requisições
 })
 
