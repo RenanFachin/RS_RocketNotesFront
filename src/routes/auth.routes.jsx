6161717ff07ef7519c9a444b0aa4c55eeaa0295c
Renan <renan.fachin@hotmail.com>
2022-08-24 18:37:17 -0300

    Bug Fix

--- src/routes/auth.routes.jsx
+++ src/routes/auth.routes.jsx
@@ -3,14 +3,14 @@ import { Routes, Route } from 'react-router-dom'
 //Route é para cada rota
 
 
-import { Signin } from '../pages/Signin'
+import { SignIn } from '../pages/Signin'
 import { SignUp } from '../pages/SignUp'
 
 
 export function AuthRoutes(){
     return(
         <Routes>
-            <Route path="/" element={<Signin />} />
+            <Route path="/" element={<SignIn />} />
             <Route path="/register" element={<SignUp />} />
         </Routes>
     )
